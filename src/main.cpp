e8a5a936b64446780d378d305c8fb08d42e21033
Hiroki Mori <yamori813@yahoo.co.jp>
2026-01-06 14:47:33 +0900

    more enlarge

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,7 @@ void printutf8(int _x, int _y, char *c)
     int i, j, k;
     ft.setFontSize(EXFONT16);
     int n;
-    uint16_t pUTF16[250];
+    uint16_t pUTF16[256];
     n = exfonts::Utf8ToUtf16(pUTF16, c);  // UTF8からUTF16に変換する
     x = _x;
     off = 0;
